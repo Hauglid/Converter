fa093f425f77d0afcb8fbe31803b8cb66f4f4ff8
Hauglid <karltha@ifi.uio.no>
2016-04-09 01:25:14 +0200

    added toolbar

--- Converter/app/src/main/java/com/hauglidtech/converter/Homescreen.java
+++ Converter/app/src/main/java/com/hauglidtech/converter/Homescreen.java
@@ -2,12 +2,17 @@ package com.hauglidtech.converter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class Homescreen extends AppCompatActivity {
+public class Homescreen extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
     }
 }
